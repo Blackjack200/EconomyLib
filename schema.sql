c3788db9d0e2b5dc0071d9ef169787ffd3306542
Blackjack200 <lx826444126@gmail.com>
2022-07-16 21:39:46 +0800

    more types

--- schema.sql
+++ schema.sql
@@ -14,7 +14,7 @@ create table if not exists `rank_display`
     primary key (`basename`)
 ) default charset = utf8;
 
-create table `player_info`
+create table if not exists `player_info`
 (
     `player_name` varchar(16) not null,
     `kill`        integer     not null default 0,
@@ -22,3 +22,12 @@ create table `player_info`
     primary key (`player_name`)
 ) default charset = utf8;
 
+create table if not exists `ban_info`
+(
+    `player_name` char(16)        not null,
+    `ban_time`    bigint unsigned not null default 0,
+    `ban_end`     bigint unsigned not null default 0,
+    `source`      char(16)        not null default 'unknown',
+    `reason`      blob(25565)     not null default 'unknown reason',
+    primary key (`player_name`)
+) default charset = utf8;
